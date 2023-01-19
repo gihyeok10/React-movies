dda877e7d792ae3c09c558ba71114211739c4fd5
kimgihyeok <gh9812301@naver.com>
2023-01-19 14:48:15 +0900

    배너(api)

--- react-netflix/src/pages/Home.js
+++ react-netflix/src/pages/Home.js
@@ -1,14 +1,20 @@
 import React,{useEffect} from 'react'
 import { movieAction } from '../redux/actions/movieAction'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import Banner from '../component/Banner'
 const Home = () => {
+      const {popularMovies,topRatedMovies,upcomingMovies,} = useSelector(state => state.movie)
+      console.log("home",popularMovies)
       const dispatch = useDispatch()
     useEffect(() => {
         dispatch(movieAction.getMovies())
     },[])
   return (
-    <div>Home</div>
+    <div>
+      {popularMovies.results && <Banner movie={popularMovies.results[0]}/>}
+      
+    </div>
   )
 }
-
+//조건부 랜더링
 export default Home
--- react-netflix/src/redux/actions/movieAction.js
+++ react-netflix/src/redux/actions/movieAction.js
@@ -3,9 +3,30 @@ const API_KEY= process.env.REACT_APP_API_KEY
 function getMovies(){
     
     return async(dispatch) => {
-        
-        const popualrMovieApi =  await api.get(`movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
 
+        const topRatedApi =  api.get(
+            `movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
+
+        const popularMoviesApi =   api.get(
+            `movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+
+        const upcomingApi =  api.get(
+            `movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`)
+
+        // let data = await Promise.all([topRatedApi,popualrMovieApi,upcomingApi])  //api한번에 보여주기 느낌.
+
+        let [topRatedMovies,popularMovies,upcomingMovies] = await Promise.all([topRatedApi,popularMoviesApi,upcomingApi])
+
+        // console.log(topRatedMovies)
+        // console.log(popualrMovies)
+        // console.log(upcomingMovies)
+
+        dispatch({
+            type:"GET_MOVIES_SUCCESS"
+            ,payload: {popularMovies:popularMovies.data ,
+                topRatedMovies:topRatedMovies.data , 
+                upcomingMovies: upcomingMovies.data },
+        })
         // let url = `https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1`
         // let respones = await fetch(url)
         // let data = respones = await respones.json()
