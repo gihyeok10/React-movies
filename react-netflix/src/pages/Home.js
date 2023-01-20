e06c81d108da09d7e47166c43c7c4c3473b20939
kimgihyeok <gh9812301@naver.com>
2023-01-20 12:06:11 +0900

    slide라이브러리

--- react-netflix/src/pages/Home.js
+++ react-netflix/src/pages/Home.js
@@ -2,6 +2,7 @@ import React,{useEffect} from 'react'
 import { movieAction } from '../redux/actions/movieAction'
 import { useDispatch, useSelector } from 'react-redux'
 import Banner from '../component/Banner'
+import MovieSlide from '../component/MovieSlide'
 const Home = () => {
       const {popularMovies,topRatedMovies,upcomingMovies,} = useSelector(state => state.movie)
       console.log("home",popularMovies)
@@ -11,8 +12,14 @@ const Home = () => {
     },[])
   return (
     <div>
-      {popularMovies.results && <Banner movie={popularMovies.results[0]}/>}
-      
+      {popularMovies.results && <Banner movie={popularMovies.results[0]}/>} 
+
+      <h1>popularMovies</h1>
+      <MovieSlide movies={popularMovies}/>
+      <h1>top rated movie</h1>
+      <MovieSlide movies={topRatedMovies}/>
+      <h1>upcoming moveie</h1>
+      <MovieSlide movies={upcomingMovies}/>
     </div>
   )
 }
